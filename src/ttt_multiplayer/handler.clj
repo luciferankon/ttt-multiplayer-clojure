831af359307968b521d7a0a0acb05386976ff3f7
luciferankon <44019225+luciferankon@users.noreply.github.com>
2020-01-20 14:25:15 +0530

    api server for TTT

--- src/ttt_multiplayer/handler.clj
+++ src/ttt_multiplayer/handler.clj
@@ -4,24 +4,45 @@
             [ring.adapter.jetty :as jetty]
             [clojure.data.json :as json]
             [ttt-multiplayer.core :as game]
-            [ring.middleware.json :refer [wrap-json-body]]))
+            [ring.util.request :as request]))
 
-(def game-state (atom {:current {:moves #{} :symbol :x} :opponent {:moves #{} :symbol :o}}))
+(defn generate-response
+  [body]
+  {
+   :status  200
+   :headers {"Access-Control-Allow-Origin" "*"}
+   :body    body
+   })
 
-(defn get-game [] (json/write-str @game-state))
+(def game-state (atom {:current {:moves #{} :symbol :x} :opponent {:moves #{} :symbol :o} :board (repeat 9 " ")}))
+
+(defn add-board
+  [game]
+  (conj game [:board (game/get-board game)]))
+
+
+(defn update-game
+  [position game-state]
+  (->> game-state
+       (game/place-move position)
+       game/change-round
+       add-board))
+
+(defn get-position [req]
+  (read-string ((json/read-str (request/body-string req)) "position")))
+
+(defn get-game [] (generate-response (json/write-str @game-state)))
 
 (defn make-move
   [request]
-  (let [position (read-string ((:body request) "position"))]
-    (if (game/game-over? @game-state)
-      "game has already finished"
-      (do (swap! game-state (partial game/place-move position))
-          (swap! game-state game/change-round)
-          (get-game)))))
+  (if (game/game-over? @game-state)
+    (generate-response "game has already finished")
+    (do (swap! game-state (partial update-game (get-position request)))
+        (get-game))))
 
 (defroutes app-routes
            (GET "/" [] (get-game))
            (POST "/makemove" req (make-move req)))
 
 (defn -main []
-  (jetty/run-jetty (wrap-json-body app-routes) {:port 3000}))
+  (jetty/run-jetty app-routes {:port 3000}))
